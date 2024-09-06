6ab3b506186b59a30b11d37c6793fd445e834f0d
faruk <2035faruk@gmail.com>
2024-09-06 17:47:34 +0600

    add function

---
+++ function.js
@@ -0,0 +1,10 @@
+function add(num1,num2){
+    let x = num1 + num2;
+    console.log("We are in function");
+    return x;
+}
+
+
+// call function
+let xyz = add(49,1);
+console.log("result: ", xyz);
--- reverseArray.js
+++ reverseArray.js
@@ -12,9 +12,25 @@ let persons = ["omar", "Nirob", "alamin", "anik", "robin", "russel"];
 // console.log(reverseArr);
 
 // for loop
-let revArray = [];
-for (let i = 0; i < persons.length; i++) {
-    console.log(persons[i]);
-    revArray.unshift(persons[i]);
+// let revArray = [];
+// for (let i = 0; i < persons.length; i++) {
+//     console.log(persons[i]);
+//     revArray.unshift(persons[i]);
+// }
+// console.log(revArray);
+
+// let country = ["BD", "Afganistan","Germany","USA","Uk"];
+// let numbers = [2,5,44,33,20,11,7];
+
+// console.log(country.sort())
+// console.log(numbers.sort())
+
+
+let carBrand = ["BMW", "Mercedes", "Lamborgini", "RolsRoyce", "Toyota"];
+
+let revCar = [];
+
+for (let i = carBrand.length - 1; i >= 0; i--) {
+    revCar.push(carBrand[i])
+    console.log(revCar);
 }
-console.log(revArray);
